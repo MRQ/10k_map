430cc227829012639944b7a5900620f7ebe13657
MRQ <mrq@aachen.ccc.de>
2017-03-05 14:05:55 +0100

    Änderungen an reveal.js aus neuerem Vortrag übernommen.

--- vortrag/plugin/notes/notes.js
+++ vortrag/plugin/notes/notes.js
@@ -4,78 +4,77 @@
  */
 var RevealNotes = (function() {
 
+	function bindKey(keyCode, callback) {
+		// Disregard the event if the target is editable or a
+		// modifier is present
+		document.addEventListener( 'keydown', function( event ) {
+			if ( document.querySelector( ':focus' ) !== null || event.shiftKey || event.altKey || event.ctrlKey || event.metaKey ) return;
+			if ( event.keyCode === keyCode ) { // reset timer on 'r'
+				event.preventDefault();
+				callback();
+			}
+		});
+	}
+
+
 	function openNotes() {
 		var jsFileLocation = document.querySelector('script[src$="notes.js"]').src;  // this js file path
 		jsFileLocation = jsFileLocation.replace(/notes\.js(\?.*)?$/, '');   // the js folder path
 		var notesPopup = window.open( jsFileLocation + 'notes.html', 'reveal.js - Notes', 'width=1120,height=850' );
 
 		// Fires when slide is changed
-		Reveal.addEventListener( 'slidechanged', function( event ) {
-			post('slidechanged');
-		} );
+		Reveal.addEventListener( 'slidechanged', post );
 
 		// Fires when a fragment is shown
-		Reveal.addEventListener( 'fragmentshown', function( event ) {
-			post('fragmentshown');
-		} );
+		Reveal.addEventListener( 'fragmentshown', post );
 
 		// Fires when a fragment is hidden
-		Reveal.addEventListener( 'fragmenthidden', function( event ) {
-			post('fragmenthidden');
-		} );
+		Reveal.addEventListener( 'fragmenthidden', post );
 
 		/**
 		 * Posts the current slide data to the notes window
-		 *
-		 * @param {String} eventType Expecting 'slidechanged', 'fragmentshown' 
-		 * or 'fragmenthidden' set in the events above to define the needed 
-		 * slideDate.
 		 */
-		function post( eventType ) {
+		function post() {
 			var slideElement = Reveal.getCurrentSlide(),
+				slideIndices = Reveal.getIndices(),
 				messageData;
 
-			if( eventType === 'slidechanged' ) {
-				var notes = slideElement.querySelector( 'aside.notes' ),
-					indexh = Reveal.getIndices().h,
-					indexv = Reveal.getIndices().v,
-					nextindexh,
-					nextindexv;
+			var notes = slideElement.querySelector( 'aside.notes' ),
+				nextindexh,
+				nextindexv;
 
-				if( slideElement.nextElementSibling && slideElement.parentNode.nodeName == 'SECTION' ) {
-					nextindexh = indexh;
-					nextindexv = indexv + 1;
-				} else {
-					nextindexh = indexh + 1;
-					nextindexv = 0;
-				}
+			if( slideElement.nextElementSibling && slideElement.parentNode.nodeName == 'SECTION' ) {
+				nextindexh = slideIndices.h;
+				nextindexv = slideIndices.v + 1;
+			} else {
+				nextindexh = slideIndices.h + 1;
+				nextindexv = 0;
+			}
 
-				messageData = {
-					notes : notes ? notes.innerHTML : '',
-					indexh : indexh,
-					indexv : indexv,
-					nextindexh : nextindexh,
-					nextindexv : nextindexv,
-					markdown : notes ? typeof notes.getAttribute( 'data-markdown' ) === 'string' : false
-				};
-			}
-			else if( eventType === 'fragmentshown' ) {
-				messageData = {
-					fragment : 'next'
-				};
-			}
-			else if( eventType === 'fragmenthidden' ) {
-				messageData = {
-					fragment : 'prev'
-				};
-			}
+			messageData = {
+				update : true,
+				reset : false,
+				notes : notes ? notes.innerHTML : '',
+				indexh : slideIndices.h,
+				indexv : slideIndices.v,
+				indexf : slideIndices.f,
+				nextindexh : nextindexh,
+				nextindexv : nextindexv,
+				markdown : notes ? typeof notes.getAttribute( 'data-markdown' ) === 'string' : false
+			};
 
 			notesPopup.postMessage( JSON.stringify( messageData ), '*' );
 		}
 
+		// Reset timer on 'r'
+		bindKey(82, function() {
+			var messageData = { update : false, reset : true };
+			notesPopup.postMessage( JSON.stringify( messageData ), '*' );
+		});
+
 		// Navigate to the current slide when the notes are loaded
 		notesPopup.addEventListener( 'load', function( event ) {
-			post('slidechanged');
+			post();
 		}, false );
 	}
 
